f968c53c51fb4b279495299664661997549c3e79
malauzet <30903872+malauzet@users.noreply.github.com>
2026-07-04 21:48:29 +0200

    Initial commit: Spring Boot backend + Angular frontend scaffolds

---
+++ bookshelf-api/src/test/java/com/malauzet/bookshelfapi/BookshelfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malauzet.bookshelfapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshelfApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
